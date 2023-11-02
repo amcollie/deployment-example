8dc65acfeff32628fc4f754efd59a064526afbc0
Alexandros Collie <amcollie@gmail.com>
2023-11-01 22:33:58 -0400

    commented out Invoices services

--- app/src/Services/InvoiceService.php
+++ app/src/Services/InvoiceService.php
@@ -8,26 +8,26 @@ use App\Contracts\PaymentGatewayInterface;
 
 class InvoiceService
 {
-    public function __construct(
-        protected SalesTaxService $salesTaxService,
-        protected PaymentGatewayInterface $paymentGateway,
-        protected EmailService $emailService
-    )
-    {
+    // public function __construct(
+    //     protected SalesTaxService $salesTaxService,
+    //     protected PaymentGatewayInterface $paymentGateway,
+    //     protected EmailService $emailService
+    // )
+    // {
 
-    }
+    // }
 
-    public function process(array $customer, float $amount): bool
-    {
+    // public function process(array $customer, float $amount): bool
+    // {
 
-        $tax = $this->salesTaxService->calculate($amount, $customer);
+    //     $tax = $this->salesTaxService->calculate($amount, $customer);
 
-        if (!$this->paymentGateway->charge($customer, $amount, $tax)) {
-            return false;
-        }
+    //     if (!$this->paymentGateway->charge($customer, $amount, $tax)) {
+    //         return false;
+    //     }
 
-        $this->emailService->send($customer, 'receipt');
-        echo 'Invoice has been processed<br/>';
-        return true;
-    }
+    //     $this->emailService->send($customer, 'receipt');
+    //     echo 'Invoice has been processed<br/>';
+    //     return true;
+    // }
 }
